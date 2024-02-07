7d9c1fd6e38c8c3f93e9128c01f33f1af678a92a
Oscar Aguirre <oscar.aguirre.martinez777@gmail.com>
2024-02-06 20:12:23 -0600

    Tares 06feb24

---
+++ AguirreMartinez06feb2024/settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Aguirre Martinez 06feb2024"
+include(":app")
